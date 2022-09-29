b158a0eb3a727c09165fd87ed3f4a4414be3c931
IlyukhinSergey <ilukhin.sergei@gmail.com>
2022-09-29 11:34:41 +0300

    added task 3.1

---
+++ task3.1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require('src/functions.php');
+
+$users = task1();
+
+task2($users);
+
+task3();
---
+++ task3.1/src/functions.php
@@ -0,0 +1,78 @@
+<?php
+
+function task1()
+{
+    $users = [];
+
+    for ($i = 0; $i < 50; $i++) {
+        $userName = rand(1, 5);
+        $userAge = rand(18, 45);
+
+        switch ($userName) {
+            case 1:
+                $name = 'Sasha';
+                break;
+            case 2:
+                $name = 'Steffi';
+                break;
+            case 3:
+                $name = 'Masha';
+                break;
+            case 4:
+                $name = 'Ivan';
+                break;
+            case 5:
+                $name = 'Sidr';
+                break;
+        }
+        $users[$i] = ['name' => $name, 'age' => $userAge];
+    }
+    return $users;
+}
+
+function task2($users)
+{
+    $json = json_encode($users);
+    file_put_contents('users.json', $json);
+}
+
+function task3()
+{
+    $users = json_decode(file_get_contents('users.json'), true);
+
+    $sasha = 0;
+    $steffi = 0;
+    $masha = 0;
+    $ivan = 0;
+    $sidr = 0;
+    $allAge = 0;
+
+    foreach ($users as $user) {
+        switch ($user['name']) {
+            case 'Sasha':
+                $sasha++;
+                break;
+            case 'Steffi':
+                $steffi++;
+                break;
+            case 'Masha':
+                $masha++;
+                break;
+            case 'Ivan':
+                $ivan++;
+                break;
+            case 'Sidr':
+                $sidr++;
+                break;
+        }
+        $allAge += $user['age'];
+    }
+
+    echo 'В массиве users количество имен:' . '<br>' .
+      'имени Sasha = ' . $sasha . '<br>' .
+      'имени Steffi = ' . $steffi . '<br>' .
+      'имени Masha = ' . $masha . '<br>' .
+      'имени Ivan = ' . $ivan . '<br>' .
+      'имени Sidr = ' . $sidr . '<br>' .
+      'Общий возраст составляет - ' . $allAge;
+}
