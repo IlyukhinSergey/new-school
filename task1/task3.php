d6e816fa918d15d871241965008181fa270f97f6
IlyukhinSergey <ilukhin.sergei@gmail.com>
2022-10-11 13:30:52 +0300

    added switch-case

--- task1/task3.php
+++ task1/task3.php
@@ -1,13 +1,17 @@
 <?php
 
-$age = 1;
+$age = 44;
 
-if ($age >= 18 && $age <= 65) {
-    echo 'Вам еще работать и работать';
-} elseif ($age > 65) {
-    echo 'Вам пора на пенсию';
-} elseif ($age >= 1 && $age <= 17) {
-    echo 'Вам ещё рано работать';
-} else {
-    echo 'Неизвестный возраст';
+switch ($age) {
+    case $age >= 1 && $age <= 17:
+        echo 'Вам ещё рано работать';
+        break;
+    case $age >= 18 && $age <= 65:
+        echo 'Вам еще работать и работать';
+        break;
+    case $age > 65:
+        echo 'Вам пора на пенсию';
+        break;
+    default:
+        echo 'Неизвестный возраст';
 }
