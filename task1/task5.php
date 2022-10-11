d1338d667e47ea7704e498ce5377e41e6119587b
IlyukhinSergey <ilukhin.sergei@gmail.com>
2022-10-11 13:43:37 +0300

    добавлен цикл в taks1.5

--- task1/task5.php
+++ task1/task5.php
@@ -4,19 +4,11 @@ $bmw = ['model' => 'X5', 'speed' => 120, 'doors' => 5, 'year' => '2015'];
 $toyota = ['model' => 'camry', 'speed' => 150, 'doors' => 4, 'year' => '2019'];
 $opel = ['model' => 'astra', 'speed' => 90, 'doors' => 3, 'year' => '2017'];
 
-$nameCars = ['bmw', 'toyota', 'opel'];
 $cars = ['bmw' => $bmw, 'toyota' => $toyota, 'opel' => $opel];
 
-$format = 'CAR %s <br> %s %s %s %s <br>';
+foreach ($cars as $key => $value) {
+    $format = 'CAR %s <br> %s %d %d %s <br>';
 
-echo sprintf($format, $nameCars[0],
-  $cars['bmw']['model'], $cars['bmw']['speed'], $cars['bmw']['doors'],
-  $cars['bmw']['year']);
-
-echo sprintf($format, $nameCars[1],
-  $cars['toyota']['model'], $cars['toyota']['speed'], $cars['toyota']['doors'],
-  $cars['toyota']['year']);
-
-echo sprintf($format, $nameCars[2],
-  $cars['opel']['model'], $cars['opel']['speed'], $cars['opel']['doors'],
-  $cars['opel']['year']);
+    echo sprintf($format, $key,
+      $value['model'], $value['speed'], $value['doors'], $value['year']);
+}
