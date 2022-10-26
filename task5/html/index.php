b5c279811f3721c93cf23bcedf200f0160e7fcf3
IlyukhinSergey <ilukhin.sergei@gmail.com>
2022-10-26 13:55:29 +0300

    add static routing

--- task5/html/index.php
+++ task5/html/index.php
@@ -1,26 +1,25 @@
 <?php
 
-use App\Controller\Blog;
-use App\Controller\User;
+use Base\Route;
 
-require '../vendor/autoload.php';
+include '../vendor/autoload.php';
 
 $parts = parse_url($_SERVER['REQUEST_URI']); // '/new-school/task5/html/'
 
-switch ($parts['path']) {
-    case '/new-school/task5/html/user/login':
-        $user = new User();
-        $user->loginAction();
-        break;
-    case '/new-school/task5/html/user/register':
-        $user = new User();
-        $user->registerAction();
-        break;
-    case '/new-school/task5/html/blog/index':
-    case '/new-school/task5/html/blog':
-        $user = new Blog();
-        $user->indexAction();
-        break;
-    default:
-        header("HTTP/1.0 404 Nit Found");
-}
+/** @uses \App\Controller\User::loginAction() */
+$rout = new Route();
+$rout->addRoute('/new-school/task5/html/user/login', \App\Controller\User::class, 'login');
+
+/** @uses \App\Controller\User::registerAction() */
+$rout->addRoute('/new-school/task5/html/user/register', \App\Controller\User::class, 'register');
+
+/** @uses \App\Controller\Blog::indexAction() */
+$rout->addRoute('/new-school/task5/html/blog/index', \App\Controller\Blog::class, 'index');
+
+$controllerName = $rout->getControllerName();
+$controller = new $controllerName;
+
+$actionName = $rout->getActionName();
+$controller->$actionName();
+
+
---
+++ task5/src/Route.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Base;
+
+use App\Controller\Blog;
+use App\Controller\User;
+
+class Route
+{
+
+    private $controllerName;
+
+    private $actionName;
+
+    private $processed = false;
+
+    private $routes;
+
+    private function process()
+    {
+        $parts = parse_url($_SERVER['REQUEST_URI']); // '/new-school/task5/html/'
+        $path = $parts['path'];
+
+        if (($rout = $this->routes[$path] ?? null) !== null) {
+            $this->controllerName = $rout[0];
+            $this->actionName = $rout[1];
+        }
+
+//        switch ($parts['path']) {
+//            case '/new-school/task5/html/user/login':
+//                $user = new User();
+//                $user->loginAction();
+//                break;
+//            case '/new-school/task5/html/user/register':
+//                $user = new User();
+//                $user->registerAction();
+//                break;
+//            case '/new-school/task5/html/blog/index':
+//            case '/new-school/task5/html/blog':
+//                $user = new Blog();
+//                $user->indexAction();
+//                break;
+//            default:
+//                header("HTTP/1.0 404 Nit Found");
+//        }
+    }
+
+    public function addRoute($path, $controllerName, $actionName)
+    {
+        $this->routes[$path] = [
+          $controllerName,
+          $actionName,
+        ];
+    }
+
+    public function getControllerName(): string
+    {
+        if (!$this->processed) {
+            $this->process();
+        }
+        return $this->controllerName;
+    }
+
+    public function getActionName(): string
+    {
+        if (!$this->processed) {
+            $this->process();
+        }
+        return $this->actionName . 'Action';
+    }
+
+}
