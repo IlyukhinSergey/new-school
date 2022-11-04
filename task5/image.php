465e830fcd171a6f11b2b13392a698abe0e12409
IlyukhinSergey <ilukhin.sergei@gmail.com>
2022-11-04 18:54:44 +0300

    added intervention/image

--- task5/image.php
+++ task5/image.php
@@ -1,3 +1,22 @@
 <?php
 
 require 'vendor/autoload.php';
+
+use Intervention\Image\ImageManagerStatic;
+
+
+$image = ImageManagerStatic::make('images/lego.jpg');
+
+$image->resize(200, null,
+  function (\Intervention\Image\Constraint $constraint) {
+      $constraint->aspectRatio();
+  });
+
+$image->text('Hello world', 40, 40,
+  function (\Intervention\Image\AbstractFont $font) {
+      $font->size(100);
+      $font->color('#000000');
+  });
+$image->save('images/new_lego.jpg');
+
+echo $image->response('jpg');
